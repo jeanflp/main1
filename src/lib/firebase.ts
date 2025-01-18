0988f8f827956ac44f472167a92d75d8d56ef0e4
Jean Felipe <42298526+jeanflp@users.noreply.github.com>
2025-01-17 22:20:58 -0300

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC7-Aigtg9RJaw9QqlK4tmmFheGZN9etqs",
-  authDomain: "factions-manager.firebaseapp.com",
-  projectId: "factions-manager",
-  storageBucket: "factions-manager.firebasestorage.app",
-  messagingSenderId: "588392686089",
-  appId: "1:588392686089:web:f9611ad4168de317200ed8",
-  measurementId: "G-VGE8TJ6J8C",
+  apiKey: "AIzaSyDmKbag5JSYpi7NTAehAKw--WL-60XyDx8",
+  authDomain: "factions-d95b2.firebaseapp.com",
+  projectId: "factions-d95b2",
+  storageBucket: "factions-d95b2.firebasestorage.app",
+  messagingSenderId: "72792151606",
+  appId: "1:72792151606:web:7d2bf2a00eb7a36e4aea97",
+  measurementId: "G-Y3CN22LG2X"
 };
 
 const app = initializeApp(firebaseConfig);
